c323117a9d6d3e4ccf8e08b0dfecf5e9b75ed7bd
Zhen-Yee <davster2@gmail.com>
2018-08-26 19:21:16 -0400

    added changing shibe button

--- shibe-frontend/src/App.js
+++ shibe-frontend/src/App.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shibeImg: [],
+      imgLoaded: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    fetch('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true/')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          shibeImg: json,
+          imgLoaded: true
+        })
+      });
+  }
+
+  handleClick() {
+    fetch('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true/')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          shibeImg: json,
+          imgLoaded: true
+        })
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        
+      <img src={this.state.shibeImg[0]} alt='Shibe Derp' />
+      <br></br>
+      <button onClick={this.handleClick}>Click me!</button>
       </div>
     );
   }
