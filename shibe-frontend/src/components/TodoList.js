883a255b3f4c423ce687b66763d0a750cde0bd65
Zhen-Yee <davster2@gmail.com>
2018-09-02 06:47:38 -0400

    changed checkbox to cards and added notebox for todos

--- shibe-frontend/src/components/TodoList.js
+++ shibe-frontend/src/components/TodoList.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Button, List } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 import { ShibaImage } from './ShibaImage';
 
 export const TodoList = props => {
     return (
-        <List>
-            <ShibaImage shibaImg={props.shiba} onCheck={props.check} isDisable={props.disableCheck}></ShibaImage> {props.todo} <Button onClick={props.delete}>Remove</Button>
-        </List>
-      );
+        <Card>
+            <Card.Content>
+                <Card.Header>{props.todo}</Card.Header>
+                <Card.Description>
+                    {props.note}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <div className='ui two buttons'>
+                    <ShibaImage shibaImg={props.shiba} onDone={props.done} isDisable={props.disableCheck}></ShibaImage>
+                    <Button onClick={props.delete} basic color='red' disabled={props.disableCheck}>
+                        Remove
+                    </Button>
+                </div>
+            </Card.Content>
+        </Card>
+    );
 }
